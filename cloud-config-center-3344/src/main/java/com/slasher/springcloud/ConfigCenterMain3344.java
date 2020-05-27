eb19f25e8fc9cb9d72ef0d3403ea4f33adf9d962
tianjie <tianjie@tianjie>
2020-05-27 10:52:54 +0800

    引入cloud-config的server端和client端

---
+++ cloud-config-center-3344/src/main/java/com/slasher/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.slasher.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author jit
+ * @date 2020/5/27
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-client3355/src/main/java/com/slasher/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.slasher.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jit
+ * @date 2020/5/27
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
+
+
