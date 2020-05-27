c1459691a062da2d13c7d70e48b97a2f6380c6bd
tianjie <tianjie@tianjie>
2020-05-27 14:00:34 +0800

    cloud-config的服务端和客户端整合引入rabbitMQ

---
+++ cloud-config-client3366/src/main/java/com/slasher/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.slasher.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jit
+ * @date 2020/5/27
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client3366/src/main/java/com/slasher/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.slasher.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jit
+ * @date 2020/5/27
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return "serverPort" + serverPort + "\t\n\n configInfo: " + configInfo;
+    }
+}
