3fde35c85715aed35c6a3ecaec41414cf635bcea
tianjie <tianjie@tianjie>
2020-05-28 16:20:08 +0800

    nacos作为配置中心，引入客户端nacos-client

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/slasher/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.slasher.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jit
+ * @date 2020/5/28
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
